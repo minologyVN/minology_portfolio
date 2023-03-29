3e6544085b1e9aceacfc4cfb1451da2aa57177ef
minology <bluewings2605@gmail.com>
2023-03-29 23:15:27 +0700

    fix: fix css for responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { ContainerFull } from "../components/common/ContainerFull";
 import { useWheelDirectionReceive } from "../hooks/useWheelDirectionReceive";
 import { fadeWithDownDirection, floatAnimation } from "../utils";
 import { ReactComponent as SvgRightArrow } from "../assets/svg/right-arrow.svg";
-import { cat_visual } from "../assets";
+import { avatar_visual } from "../assets";
 import { Canvas } from "@react-three/fiber";
 import { MeshDistortMaterial, OrbitControls, Sphere } from "@react-three/drei";
 
@@ -30,9 +30,9 @@ export const Home = () => {
     >
       <ContainerFull className="relative flex items-center justify-center">
         <div className="flex flex-col ml-64 mr-48 w-full h-full pl-8">
-          <section className="relative flex items-end basis-2/3 border-b-2 gap-2 border-white-opacity">
-            <div className="relative py-10 pr-48">
-              <div className="mb-8 font-heading font-extraBold capitalize pr-52 tracking-widest">
+          <section className="relative flex justify-center items-center basis-2/3 border-b-2 gap-2 border-white-opacity mx-24">
+            <div className="w-1/2 ml-20">
+              <div className="w-full mb-8 font-heading font-extraBold capitalize tracking-widest">
                 <HomeHeading>
                   <b className="text-h1">Ri! O Om</b>
                   <br />
@@ -45,13 +45,16 @@ export const Home = () => {
                 <SvgRightArrow width={24} height={24} />
               </ButtonHoverEffect>
             </div>
-            <div className="absolute inset-0 left-1/2 h-full">
+            <div className="relative w-1/2 h-full">
               <motion.div {...floatAnimation()} className="w-full h-full">
-                <Canvas camera={{ fov: 25, position: [5, 5, 5] }}>
+                <Canvas
+                  camera={{ fov: 25, position: [5, 5, 5] }}
+                  className="absolute inset-0"
+                >
                   <OrbitControls enableZoom={false} autoRotate />
                   <ambientLight intensity={1} />
                   <directionalLight position={[3, 2, 1]} />
-                  <Sphere args={[0.75, 150, 220]} scale={2}>
+                  <Sphere args={[0.75, 150, 220]} scale={1.4}>
                     <MeshDistortMaterial
                       color="#3D4677"
                       attach="material"
@@ -60,18 +63,18 @@ export const Home = () => {
                     />
                   </Sphere>
                 </Canvas>
-                <div className="absolute inset-0 pointer-events-none">
+                {/* <div className="absolute inset-0 pointer-events-none">
                   <img
-                    src={cat_visual}
+                    src={avatar_visual}
                     alt="home-canvas"
-                    className="w-full h-full object-cover rounded-full scale-75"
+                    className="w-full h-full object-cover rounded-full scale-[0.4] md:scale-[0.6] lg:scale-[0.7] 2xl:scale-75"
                   />
-                </div>
+                </div> */}
               </motion.div>
             </div>
           </section>
           <section className="basis-1/3 flex items-center pt-8 py-12 justify-between">
-            <span>A</span>
+            <div></div>
             <span>B</span>
             <span>C</span>
           </section>
